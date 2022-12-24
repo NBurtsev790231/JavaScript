3d1ca0c78177b84a765ed86c449e069c1fb858bc
NBurtsev790231 <nburtsev7922@gmail.com>
2022-12-24 21:20:05 +0300

    practice

---
+++ practice.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let a = 5;
+let b = 10;
+function sum(a, b) {
+	let c = a + b;
+	console.log(c);
+}
+sum(a, b);
+
+a = 8;
+b = 12;
+sum(a, b);
+
+let c = 2;
+let d = 50;
+function result(c, d) {
+	let e = c * d;
+	console.log(e);
+}
+result(c, d);
+
+function myFn(e, f) {
+	let c;
+	e = e + 1;
+	c = e + f;
+	return c;
+}
+console.log(myFn(10, 3));
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7,]
+console.log(arr);
+arr.push('a', 'b', 'c',);
+console.log(arr);
+
+
