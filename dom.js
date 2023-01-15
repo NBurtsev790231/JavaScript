a4ec5b1e87854beffb2d9620ecbfac4011ee2efb
NBurtsev790231 <nburtsev7922@gmail.com>
2023-01-15 21:46:07 +0300

    DOM

--- dom.js
+++ dom.js
@@ -1,6 +1,7 @@
 'use strict'
 
 console.log(navigator.userAgent);
+
 if (navigator.userAgent.includes('Chrome')) {
 	console.log('Browser Chrome');
 } else if (navigator.userAgent.includes('Firefox')) {
@@ -18,4 +19,32 @@ console.log(confirmResult);
 */
 console.log(location.href);
 console.log(navigator.language);
+
+const htmlElement = document.documentElement;
+const headElement = document.head;
+const bodyElement = document.body;
+console.log(htmlElement);
+//console.log(headElement);
+//console.log(bodyElement);
+
+const firstBodyElement = bodyElement.firstChild;
+const lastBodyElement = bodyElement.lastChild;
+console.log(firstBodyElement);
+console.log(lastBodyElement);
+
+const childNodesBody = bodyElement.childNodes;
+console.log(childNodesBody);
+
+console.log(bodyElement.hasChildNodes());
+
+const previousNode = bodyElement.previousSibling;
+const nextNode = bodyElement.nextSibling;
+const userParentNode = bodyElement.parentNode;
+console.log(previousNode);
+console.log(nextNode);
+console.log(userParentNode);
+
+const bodyChildren = bodyElement.children;
+console.log(bodyChildren);
+
 
