2480a21e33d9173713a90718ca8845007f586313
NBurtsev790231 <nburtsev7922@gmail.com>
2023-01-15 21:10:37 +0300

    DOM

---
+++ dom.js
@@ -0,0 +1,21 @@
+'use strict'
+
+console.log(navigator.userAgent);
+if (navigator.userAgent.includes('Chrome')) {
+	console.log('Browser Chrome');
+} else if (navigator.userAgent.includes('Firefox')) {
+	console.log('Browser Firefox');
+}
+
+/*
+alert('Hello');
+
+const promptResult = prompt('why are you')
+console.log(promptResult);
+
+const confirmResult = confirm('Document object model');
+console.log(confirmResult);
+*/
+console.log(location.href);
+console.log(navigator.language);
+
